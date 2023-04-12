a4f46eadb9ccd2a22661a4d18df64bc2e9be258a
KeremTEKER <teker.kerem@gmail.com>
2023-04-12 21:09:34 +0300

    httpclientmodule added.

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
+import { HttpClient } from '@angular/common/http';
+import { MovieResponseModel } from 'src/app/models/movieResponseModel';
 
 @Component({
   selector: 'app-movie',
@@ -7,13 +9,19 @@ import { Movie } from 'src/app/models/movie';
   styleUrls: ['./movie.component.css'],
 })
 export class MovieComponent implements OnInit {
-  
-
-  movies:Movie[] = [];
-  constructor() { }
+  movies: Movie[] = [];
+  apiUrl = 'https://localhost:44358/api/movies/getAll';
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-
+    this.getMovies();
   }
 
+  getMovies() {
+    this.httpClient
+      .get<MovieResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.movies = response.data
+      });
+  }
 }
